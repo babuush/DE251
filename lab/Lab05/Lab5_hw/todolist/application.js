d7c0346651b9e6ff4241f39f3b07c2c9ee561ad3
babuush <thanapatpiyamassakul@gmail.com>
2024-09-08 11:51:13 +0700

    Update Lab5 homework files

--- lab/Lab05/Lab5_hw/todolist/application.js
+++ lab/Lab05/Lab5_hw/todolist/application.js
@@ -2,14 +2,16 @@ const addButton = document.getElementById("addTask");
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+loadTasks();
+
 function addTask() {
     const task = taskInput.value.trim();
     if (task) {
-        createTaskElement(task)
+        createTaskElement(task);
         taskInput.value = '';
-        saveTask()
+        saveTask();
     } else {
-        alert("Enter a task");
+        alert("Please enter a task.");
     }
 }
 
@@ -17,14 +19,31 @@ addButton.addEventListener("click", addTask);
 
 function createTaskElement(task) {
     const listItem = document.createElement("li");
+    listItem.className = "list-group-item";
     listItem.textContent = task;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "deleteTask btn btn-danger btn-sm";
+    listItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", function () {
+        taskList.removeChild(listItem);
+        saveTask();
+    });
+
     taskList.appendChild(listItem);
 }
 
 function saveTask() {
     let tasks = [];
     taskList.querySelectorAll("li").forEach(function (item) {
-        tasks.push(item.textContent.trim());
+        tasks.push(item.textContent.replace('Delete', '').trim());
     });
     localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(createTaskElement);
 }
